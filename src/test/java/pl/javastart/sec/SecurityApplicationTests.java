2ffa82b5f7a0d3937a6b9d30353d3b106cc2533c
Slawek Ludwiczak <slawekwch@gmail.com>
2023-01-26 11:04:38 +0100

    fixed parameter names, but tests are still inconsistent with the behaviour of running application

--- src/test/java/pl/javastart/sec/SecurityApplicationTests.java
+++ src/test/java/pl/javastart/sec/SecurityApplicationTests.java
@@ -8,6 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -21,8 +22,9 @@ class SecurityApplicationTests {
 	void worksWithSpringBoot2_7_5() throws Exception {
 		mockMvc.perform(post("/login")
 						.contentType(MediaType.APPLICATION_FORM_URLENCODED)
-						.param("user", "admin")
+						.param("username", "admin")
 						.param("password", "hardPassword"))
+//						.with(csrf()))
 				.andExpect(status().isForbidden());
 	}
 
@@ -30,7 +32,7 @@ class SecurityApplicationTests {
 	void worksWithSpringBoot3_0_2() throws Exception {
 		mockMvc.perform(post("/login")
 						.contentType(MediaType.APPLICATION_FORM_URLENCODED)
-						.param("user", "admin")
+						.param("username", "admin")
 						.param("password", "hardPassword"))
 				.andExpect(status().isFound());
 	}
